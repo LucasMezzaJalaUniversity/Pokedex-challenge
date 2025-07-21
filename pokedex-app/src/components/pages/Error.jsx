6f2ecb082960f5ebbcf8f4fb38c65002c3f0360c
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-07-21 15:44:42 -0300

    add Error component

---
+++ pokedex-app/src/components/pages/Error.jsx
@@ -0,0 +1,8 @@
+export const Error = () => {
+  return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "5em"}}>
+      <h1>Error</h1>
+      <p>Something went wrong while loading the page. Please try again later.</p>
+    </div>
+  );
+}
--- pokedex-app/src/router.jsx
+++ pokedex-app/src/router.jsx
@@ -5,6 +5,7 @@ import { Types } from "./components/templates/Types";
 import { Generations } from "./components/templates/Generations";
 import { getRequestFromAPI } from "./api/api";
 import App from "./App";
+import { Error } from "./components/pages/Error";
 
 const pokemonLoader = async () => {
   const data = await getRequestFromAPI(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`);
@@ -12,13 +13,14 @@ const pokemonLoader = async () => {
   const pokemonsDetail = await Promise.all(pokemonsUrl);
   return pokemonsDetail;
 };
+
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
       { path: "/", element: <Home />, loader: pokemonLoader },
-      { path: "game", element: <Game /> },
+      { path: "game", element: <Game />, errorElement: <Error /> },
       { path: "langs", element: <section>Langs</section> },
       { path: "types", element: <Types /> },
       { path: "generations", element: <Generations /> },
