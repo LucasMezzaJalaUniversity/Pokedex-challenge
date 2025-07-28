2a1e2b72ff795fe249e8741f49f98308b060c8c4
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-07-28 12:29:03 -0300

    add stats info

--- pokedex-app/src/components/molecules/Stats.jsx
+++ pokedex-app/src/components/molecules/Stats.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { DetailPokemonContext } from "../../contexts/DetailPokemonContext";
+import './Stats.css'
+
 export const Stats = () => {  
+  const { detailPokemon } = useContext(DetailPokemonContext);
+  
   return (
-    <div>
-      <h2>Stats</h2>
-    </div>
+    (!detailPokemon || Object.values(detailPokemon).length === 0 || detailPokemon.stats.length === 0) ? 
+      <p>There is not stats available</p> : (
+      <div className="stat">
+        <ul>
+          {detailPokemon.stats.map((row, idx) => (
+            <li key={idx}>
+              <em>{row.stat.name}</em>
+              <span>{row.base_stat}</span>
+              <div className="container">
+                <div style={{ width: `${row.base_stat}%` }} className="stat-bar"></div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
   );
 }
